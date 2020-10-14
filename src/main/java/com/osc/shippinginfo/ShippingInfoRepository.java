9de7db8733d1edca96373b6d568883347a56458a
isirlincoln <44763135+isirlincoln@users.noreply.github.com>
2020-10-14 14:07:55 -0400

    Added Shipping Information
    Created Model for shipping information

---
+++ src/main/java/com/osc/shippinginfo/ShippingInfoRepository.java
@@ -0,0 +1,7 @@
+package com.osc.shippinginfo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShippingInfoRepository extends JpaRepository<ShippingInfo, Integer> {
+
+}
